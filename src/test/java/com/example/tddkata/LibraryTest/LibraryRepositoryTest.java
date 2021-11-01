97e5445b13b28b623afa96d70b6d61ab94b94b38
Piotr Rogowski <233998@edu.p.lodz.pl>
2021-11-01 20:59:16 +0100

    LibraryRepository - whenFindByName_ReturnsLibrary - failed - implementation of code needed

---
+++ src/test/java/com/example/tddkata/LibraryTest/LibraryRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.example.tddkata.LibraryTest;
+
+import com.example.tddkata.dao.Library;
+import com.example.tddkata.repository.LibraryRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class LibraryRepositoryTest {
+
+    @Autowired
+    private LibraryRepository libraryRepository;
+
+    @Test
+    public void whenFindByName_ReturnsLibrary(){
+        Library library = libraryRepository.findByName("libok");
+
+        Assertions.assertThat(library.getName()).isEqualTo("libok");
+        Assertions.assertThat(library.isPrivate()).isEqualTo(false);
+    }
+}
